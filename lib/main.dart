7d2b961c2c5e0eb589589d63bc671aa816745f62
Brian <aladebrian@gmail.com>
2025-03-09 23:15:48 -0400

    created calendar buttons and drag and drop functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'plan.dart';
 
@@ -29,26 +31,44 @@ class PlanManagerScreen extends StatefulWidget {
 
 class _PlanManagerScreenState extends State<PlanManagerScreen> {
   List<Plan> plans = [Plan(name: "Plan 1", description: "This is plan 1")];
+  Map<int, List<Plan>> orderedPlans = {};
   TextEditingController nameTextController = TextEditingController();
   TextEditingController descriptionTextController = TextEditingController();
-
+  List<String> days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+  String currentView = "month";
+  int currentDay = -1;
   Draggable draggableListTile(index) {
-    return Draggable<GestureDetector>(
-      feedback: listTilePlan(index),
+    return Draggable<Material>(
+      feedback: unorderedListTilePlan(index),
       child: GestureDetector(
         onDoubleTap: () => changeName(index),
-        child: listTilePlan(index),
+        child: SizedBox(
+          width: 250,
+          height: 300,
+          child: unorderedListTilePlan(index),
+        ),
       ),
     );
   }
 
-  Material listTilePlan(index) {
+  Material unorderedListTilePlan(index) {
     return Material(
-      child: ListTile(
-        title: Text(plans[index].name),
-        subtitle: Text(plans[index].description),
-        tileColor: plans[index].completed ? Colors.green : Colors.red,
+      child: SizedBox(
+        width: 250,
+        height: 50,
+        child: ListTile(
+          title: Text(plans[index].name),
+          tileColor: plans[index].completed ? Colors.green : Colors.red,
+        ),
       ),
+    );
+  }
+
+  ListTile orderedListTilePlan(index) {
+    return ListTile(
+      title: Text(orderedPlans[currentDay]![index].name),
+      subtitle: Text(orderedPlans[currentDay]![index].description),
+      tileColor: plans[index].completed ? Colors.green : Colors.red,
     );
   }
 
@@ -58,7 +78,7 @@ class _PlanManagerScreenState extends State<PlanManagerScreen> {
     });
   }
 
-  void addPlan() {
+  void addUnorderedPlan() {
     setState(() {
       plans.add(
         Plan(
@@ -69,59 +89,111 @@ class _PlanManagerScreenState extends State<PlanManagerScreen> {
     });
   }
 
+  void addOrderedPlan(int date, Plan plan) {
+    setState(() {
+      if (orderedPlans.containsKey(date)) {
+        orderedPlans[date]!.add(plan);
+      } else {
+        orderedPlans[date] = [plan];
+      }
+    });
+  }
+
+  void seeDay(index) {
+    setState(() {
+      currentView = 'day';
+      currentDay = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: Row(
         children: [
-          Column(
-            children: [
-              SizedBox(
-                width: 300,
-                height: 200,
-                child: TextField(
-                  controller: nameTextController,
-                  decoration: InputDecoration(hintText: "name"),
+          SizedBox(
+            width: 300,
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 300,
+                  height: 200,
+                  child: TextField(
+                    controller: nameTextController,
+                    decoration: InputDecoration(hintText: "name"),
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 300,
-                height: 200,
-                child: TextField(
-                  controller: descriptionTextController,
-                  decoration: InputDecoration(hintText: "description"),
+                SizedBox(
+                  width: 300,
+                  height: 200,
+                  child: TextField(
+                    controller: descriptionTextController,
+                    decoration: InputDecoration(hintText: "description"),
+                  ),
                 ),
-              ),
-              
-            ],
-            
+              ],
+            ),
           ),
-          Column(
-            children: [
-              TextButton(onPressed: addPlan, child: Text("Add Plan")),
-              SizedBox(
-                width: 300,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: plans.length,
-                  itemBuilder: (context, index) {
-                    return draggableListTile(index);
-                  },
+          SizedBox(
+            width: 300,
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: addUnorderedPlan,
+                  child: Text("Add Plan"),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: plans.length,
+                    itemBuilder: (context, index) {
+                      return draggableListTile(index);
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
-          // SizedBox(
-          //   child: GridView.builder(
-          //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          //       crossAxisCount: 4,
-          //     ),
-          //     itemBuilder: (context, index) {
-          //       return SizedBox();
-          //     },
-          //   ),
-          // ),
+          if (currentView == 'month')
+            SizedBox(
+              width: 300,
+              child: GridView.builder(
+                itemCount: 37,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 7,
+                ),
+                itemBuilder: (context, index) {
+                  if (index < 7) {
+                    return Text(days[index]);
+                  } else {
+                    return TextButton(
+                      onPressed: () => seeDay(index),
+                      child: Text("${index - 6}"),
+                    );
+                  }
+                },
+              ),
+            ),
+          if (currentView == 'day')
+            SizedBox(
+              width: 300,
+              child: Column(
+                children: [
+                  TextButton(
+                    onPressed: addUnorderedPlan,
+                    child: Text("Add Plan"),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: orderedPlans[currentDay]!.length,
+                      itemBuilder: (context, index) {
+                        return unorderedListTilePlan(index);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
         ],
       ),
     );
